4eb0bdcab9cbebb49643f2f032a24f55c16ed5cf
leonardo oteca <leonardo@sinchicreaciones.com>
2019-05-11 19:10:17 -0500

    course at 50%

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
+const $inputs = document.getElementsByTagName('input');
 
-  console.log('ola perras')
+
+function validate(){
+    let isOk = false;
+    Array.from($inputs).forEach(item => {
+        validateInput(item,isOk);
+        item.addEventListener('keypress', () => item.classList.remove('is-invalid'))
+    });
+    console.log(isOk);    
+}
+
+function validateInput(item,isOk){
+    if (item.value === "") {
+        item.classList.add('is-invalid');
+        isOk = false
+    } else 
+    isOk = true
+}
